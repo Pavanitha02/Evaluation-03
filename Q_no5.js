aff84161ff527ed411135fa64b769f1365c16f91
Pavanitha02 <kalasanivenkat764@gmail.com>
2025-11-23 19:49:01 +0530

    ADD PART A files

---
+++ Q_no5.js
@@ -0,0 +1,12 @@
+let user={
+    name:"Alice",
+    address:{
+        city:"Bangaluru",
+        pin:560001,
+        geo:{lat:11.22,ing:77.33}
+    }
+
+    };
+    console.log(user.address.city);
+    console.log(user.address.geo.lat);
+    console.log(user.address.geo.ing);
---
+++ Q_no7.js
@@ -0,0 +1,8 @@
+let emp={
+    name:"Pavanitha",
+    datails:{
+        department:"IT",
+        profile:{role:"Developer"}
+    }
+};
+console.log(emp?.datails?.profile?.role);
